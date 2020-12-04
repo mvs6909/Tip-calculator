961ec2d7b90c54949e677db32f601a7b2122ca4d
manav <manav.shah8758@gmail.com>
2020-12-05 04:38:29 +0530

    Initial Commit

---
+++ Tip calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip calculator
+//
+//  Created by Manav on 05/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
